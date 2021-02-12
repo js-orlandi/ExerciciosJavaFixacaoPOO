57c708b358f5991b42e31d022cf26b8a2ad540fd
Juliano S Orlandi <js.orlandi.dev@gmail.com>
2021-02-12 20:41:13 -0300

    first commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,41 @@
 package application;
 
-public class Program {
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Product;
+
+public class program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Product product = new Product();
+		System.out.println("Enter product data: ");
+		System.out.print("name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+
+		System.out.println("Product data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+
+		System.out.println();
+		System.out.println("Updated data: " + product);
+
+		sc.close();
 
 	}
 
